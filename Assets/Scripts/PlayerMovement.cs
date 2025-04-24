67c9186f53d68288c2fd077ece51d87afef67546
sho-ikeda1 <sho.iked@bumail.net>
2025-04-24 15:41:38 +0700

    add jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rd2d;
+    Rigidbody2D rd2d; //private variable
 
-    float move;
+    //walk left-right
+    float move; //store input form player
     [SerializeField] float speed;
+
+    //jump
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isJumping;
+
 
        void Start()
     {
@@ -15,8 +21,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //walk left-right
         move = Input.GetAxis("Horizontal");
-
         rd2d.linearVelocity = new Vector2(move * speed, rd2d.linearVelocity.y);
+
+        //jump
+        if (Input.GetButtonDown("Jump") && !isJumping )
+        {
+            rd2d.AddForce(new Vector2 (rd2d.linearVelocity.x, jumpForce));
+            Debug.Log("Jump!"); //for debugging
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) 
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
     }
 }
